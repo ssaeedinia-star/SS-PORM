97964bcc81264ed941baa6ff46549b6e89a1425a
ssaeedinia-star <ssaeedina@gmail.com>
2026-09-24 22:00:17 +0330

    Update server.py

--- server.py
+++ server.py
@@ -170,18 +170,19 @@ def upload_file(patient_code):
     if not file or not file.filename:
         return jsonify({"error": "No file selected"}), 400
 
-    patient_code_safe = secure_filename(patient_code)
+        patient_code_safe = secure_filename(patient_code)
     filename = secure_filename(file.filename)
-        file_title = request.form.get("title", "").strip()
+    file_title = request.form.get("title", "").strip()
     file_date = request.form.get("date", "").strip()
 
     persian_digits = "۰۱۲۳۴۵۶۷۸۹"
     english_digits = "0123456789"
-    file_date = file_date.translate(str.maketrans(persian_digits, english_digits))
+    file_date = file_date.translate(
+        str.maketrans(persian_digits, english_digits)
+    )
 
     file_type = request.form.get("type", "").strip()
     file_stage = request.form.get("stage", "").strip()
-    object_key = f"{patient_code_safe}/{filename}"
     object_key = f"{patient_code_safe}/{filename}"
 
     s3.upload_fileobj(
